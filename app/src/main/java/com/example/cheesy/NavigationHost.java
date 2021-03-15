9c81a78e04f3bcbf53cb7cf8bb139b69314471e0
Oliver Mcglinchey <o_mcglinchey@hotmail.com>
2021-03-15 15:59:04 +0000

    First commit for Cheesy app

---
+++ app/src/main/java/com/example/cheesy/NavigationHost.java
@@ -0,0 +1,12 @@
+package com.example.cheesy;
+
+import androidx.fragment.app.Fragment;
+
+public interface NavigationHost {
+    /**
+     * Trigger a navigation to the specified fragment, optionally adding a transaction to the back
+     * stack to make this navigation reversible.
+     */
+    void navigateTo(Fragment fragment, boolean addToBackstack);
+}
+
